6a8b7af9de68eaed2d69c46a227c636604a4c4ac
cnaver1 <cnaver1@lsu.edu>
2019-10-27 22:40:58 -0500

    Made insert into one generic function. Still working on login authentication.

--- db_functions.cpp
+++ db_functions.cpp
@@ -1,112 +1,163 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sqlite3.h> 
+#include <sqlite3.h>
 #include <iostream>
 #include <string>
 using namespace std;
 
-
 static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
-   int i;
-   for(i = 0; i<argc; i++) {
-      printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-   }
-   printf("\n");
-   return 0;
+    int i;
+    //prints table info
+    for(i = 0; i<argc; i++) {
+        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+    }
+    return 0;
 }
 
-int add_user(int id, string username, string password) {
-   sqlite3 *db;
-   char *zErrMsg = 0;
-   int rc;
-   const char *sql;
-
-   /* Open database */
-   rc = sqlite3_open("vcap.db", &db);
-   
-   if( rc ) {
-      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-      return(0);
-   } else {
-      fprintf(stderr, "Opened database successfully\n");
-   }
-   /* Create SQL statement */
-   string temp = "INSERT INTO USERS VALUES ("+to_string(id)+",'"+username+"','"+password+"');";
-   const char *line1 = temp.c_str();
-   sql = line1;         
-   /* Execute SQL statement */
-   rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
-   
-   if( rc != SQLITE_OK ){
-      fprintf(stderr, "SQL error: %s\n", zErrMsg);
-      sqlite3_free(zErrMsg);
-   } else {
-      fprintf(stdout, "Added User Successfully\n");
-   }
-   sqlite3_close(db);
-   return 0;
+//insert function for all inserts
+int insert(string table, string query)
+{
+    sqlite3 *db;
+    char *zErrMsg = 0;
+    int rc;
+    const char *sql;
+    
+    /* Open database */
+    rc = sqlite3_open("vcap.db", &db);
+    
+    if( rc ) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        return(0);
+    } else {
+        fprintf(stderr, "Opened database successfully\n");
+    }
+    /* Create SQL statement */
+    string temp = "INSERT INTO " +table+ " VALUES ("+query+");";
+    cout<<temp<<endl;
+    const char *line1 = temp.c_str();
+    sql = line1;
+    /* Execute SQL statement */
+    rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+    
+    if( rc != SQLITE_OK ){
+        fprintf(stderr, "SQL error: %s\n", zErrMsg);
+        sqlite3_free(zErrMsg);
+    } else {
+        fprintf(stdout, "Added Successfully\n");
+    }
+    sqlite3_close(db);
+    return 0;
 }
 
-int add_disease(int id, string name) {
-   sqlite3 *db;
-   char *zErrMsg = 0;
-   int rc;
-   const char *sql;
-
-   /* Open database */
-   rc = sqlite3_open("vcap.db", &db);
-   
-   if( rc ) {
-      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-      return(0);
-   } else {
-      fprintf(stderr, "Opened database successfully\n");
-   }
-   /* Create SQL statement */
-   string temp = "INSERT INTO DISEASES VALUES ("+to_string(id)+",'"+name+"');";
-   const char *line1 = temp.c_str();
-   sql = line1;         
-   /* Execute SQL statement */
-   rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
-   
-   if( rc != SQLITE_OK ){
-      fprintf(stderr, "SQL error: %s\n", zErrMsg);
-      sqlite3_free(zErrMsg);
-   } else {
-      fprintf(stdout, "Added Disease Successfully\n");
-   }
-   sqlite3_close(db);
-   return 0;
+//User authortication
+//still working on this
+int user_auth(string username, string password)
+{
+    sqlite3 *db;
+    char *zErrMsg = 0;
+    int rc;
+    const char *sql;
+    bool login = false;
+    
+    /* Open database */
+    rc = sqlite3_open("vcap.db", &db);
+    
+    /* Create SQL statement */
+    string temp = "SELECT username, password from users where username = '"+username+"' and password = '"+password+"'";;
+    const char *line1 = temp.c_str();
+    sql = line1;
+    /* Execute SQL statement */
+    rc = sqlite3_exec(db, sql,callback,0, &zErrMsg);
+    cout<<rc<<endl;
+    if(rc == 0)
+    {
+        fprintf(stdout,"Login Successful.\n");
+        login = true;
+    }
+    else
+    {
+        fprintf(stdout,"Incorrect Username or Password.\n");
+        login = false;
+    }
+    sqlite3_close(db);
+    return 0;
 }
 
-int add_diagnose(int id, string date, int user, int disease) {
-   sqlite3 *db;
-   char *zErrMsg = 0;
-   int rc;
-   const char *sql;
-
-   /* Open database */
-   rc = sqlite3_open("vcap.db", &db);
-   
-   if( rc ) {
-      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-      return(0);
-   } else {
-      fprintf(stderr, "Opened database successfully\n");
-   }
-   /* Create SQL statement */
-   string temp = "INSERT INTO DIAGNOSES VALUES ("+to_string(id)+",'"+date+"',"+to_string(user)+","+to_string(disease)+");";
-   const char *line1 = temp.c_str();
-   sql = line1;         
-   /* Execute SQL statement */
-   rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
-   
-   if( rc != SQLITE_OK ){
-      fprintf(stderr, "SQL error: %s\n", zErrMsg);
-      sqlite3_free(zErrMsg);
-   } else {
-      fprintf(stdout, "Added Diagnoses Successfully\n");
-   }
-   sqlite3_close(db);
-   return 0;
+int main()
+{
+    //user_auth("nurse","password1");
+    //string sql;
+    //string query;
+    
+    //insert for users
+    /*
+     cout<<"Enter ID:";
+     cin>>query;
+     sql.append(query);
+     sql.append(",");
+     cout<<"Enter Name:";
+     sql.append("'");
+     cin>>query;
+     sql.append(query);
+     sql.append("'");
+     sql.append(",");
+     cout<<"Enter password:";
+     cin>>query;
+     sql.append("'");
+     sql.append(query);
+     sql.append("'");
+     insert("users",sql);
+     sql = "";
+     */
+    
+    //insert for disease
+    /*
+     cout<<"Enter ID:";
+     cin>>query;
+     sql.append(query);
+     sql.append(",");
+     cout<<"Enter Name:";
+     sql.append("'");
+     cin>>query;
+     sql.append(query);
+     sql.append("'");
+     insert("diseases",sql);
+     sql="";
+     */
+    
+    //insert for diagnoses
+    /*
+     cout<<"Enter ID: ";
+     cin>>query;
+     sql.append(query);
+     sql.append(",");
+     cout<<"Enter Date: ";
+     sql.append("'");
+     cin>>query;
+     sql.append(query);
+     sql.append("'");
+     sql.append(",");
+     cout<<"Enter Nurse ID: ";
+     sql.append("'");
+     cin>>query;
+     sql.append(query);
+     sql.append("'");
+     sql.append(",");
+     cout<<"Enter Disease ID: ";
+     sql.append("'");
+     cin>>query;
+     sql.append(query);
+     sql.append("'");
+     insert("Diagnoses",sql);
+     sql="";
+     */
 }
+
+
+
+
+
+
+
+
+
