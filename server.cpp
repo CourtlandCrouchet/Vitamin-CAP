8aec7f0282993644dd67c279410a2ca67386c10b
Afolabi <aige1@lsu.edu>
2019-11-17 13:04:44 -0600

    re-wrote server.cpp instead of using hospital.cpp

--- server.cpp
+++ server.cpp
@@ -1,3 +1,6 @@
+// Author: Afolabi
+// Updated: 17-Nov-2019
+// Description: Setup a server that can send a message to multiple clients
 #include <stdio.h>
 #include <sys/types.h> 
 #include <sys/socket.h>
@@ -7,19 +10,43 @@
 #include <unistd.h>
 #include <string>
 #include<bits/stdc++.h> 
+#include <pthread.h>
+#include <iostream>
+#include <arpa/inet.h>
 
 using namespace std;
 
-void error(char *msg)
+void error(string msg)
 {
-    perror(msg);
+    perror(msg.c_str());
     exit(1);
+}
+
+struct Client{
+    int fd;
+    char ip[INET_ADDRSTRLEN];
+    unsigned short port;
+    string message;
+};
+
+void *Thread(void *args) {
+    Client *clientinfo=(Client *)args;
+    pthread_t myid=pthread_self();
+    printf("Thread %p is alive and serving new client @ %s:%d\n",myid, clientinfo->ip, clientinfo->port);
+    //Send message to client
+    write(clientinfo->fd, clientinfo->message.c_str(), clientinfo->message.size());
+    printf("Thread %p closed connection with client, dying.\n", myid);
+    free(args);
+    printf("Press q to quit or any other key to accept more clients\n");
+    return NULL;
 }
 
 void SetupHospital(int port, string message){
     int sockfd, newsockfd; unsigned int clilen;
     struct sockaddr_in serv_addr, cli_addr;
     int n;
+    char in;
+    pthread_t client_thread;
     //create a socket
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) 
@@ -30,36 +57,39 @@ void SetupHospital(int port, string message){
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(port);
     //Bind the socket to a port
-    if (bind(sockfd, (struct sockaddr *) &serv_addr,
-            sizeof(serv_addr)) < 0) 
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
             error("ERROR on binding");
     //listen for client connection on the port
-    listen(sockfd,5);
-    clilen = sizeof(cli_addr);
-    printf("Waiting on client to connect...\n");
-    //accept a connection from client
-    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-    if (newsockfd < 0) 
-          error("ERROR on accept");
-    // send message over established connection
-    n = write(newsockfd,message.c_str(),message.size());
-    if (n < 0) error("ERROR writing to socket");
-    printf("Sent out CAP message!\n");
+    listen(sockfd,20);
+    
+    
+    while(1){
+        Client* customer = new Client;
+        cout << "Now accepting clients..." << endl;
+        //Clear out space for client
+        bzero(&cli_addr, sizeof(cli_addr));
+        clilen = sizeof(cli_addr);
+        //accept a connection from client
+        newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+        //Get client IP address
+        inet_ntop(AF_INET, &(cli_addr.sin_addr), customer->ip, INET_ADDRSTRLEN);
+        //Set relevant client info to be passed to thread
+        customer->port=cli_addr.sin_port;
+        customer->fd=newsockfd;
+        customer->message=message;
+        //Create thread and pass all info to it
+        pthread_create(&client_thread, NULL, Thread, customer);
+        //option to quit
+        cin >> in;
+        if(in == 'q'){
+            return;
+        }  
+    }
 }
 
 int main(int argc, char *argv[])
 {
-    //Read from a file and put it in a string
-    std::ifstream t("file.txt");
-    std::string input((std::istreambuf_iterator<char>(t)),
-                    std::istreambuf_iterator<char>());
-    
-    if (argc < 2) {
-        fprintf(stderr,"ERROR, no port provided\n");
-        exit(1);
-    }
-    
-    //Function accepts a port no and a message to send
-    SetupHospital(atoi(argv[1]),input);
+    //Function accepts a message to send
+    SetupHospital(atoi(argv[1]),"Testing 1,2");
     return 0; 
 }
